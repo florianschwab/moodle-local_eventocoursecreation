5de575e8c573e11ad619b5b8f29bd68359b40bed
barrasroger <roger.barras@htwchur.ch>
2018-01-23 13:32:41 +0100

    fix #3: wrong state for canceled events

--- version.php
+++ version.php
@@ -24,12 +24,12 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2017211101; // The current module version (Date: YYYYMMDDXX)
+$plugin->version   = 2017211102; // The current module version (Date: YYYYMMDDXX)
 $plugin->requires  = 2016120500; // Requires this Moodle version
 // $plugin->cron      = 0;          // Period for cron to check this module (secs)
 $plugin->component = 'local_eventocoursecreation'; // Full name of the plugin (used for diagnostics)
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "1.0"; // User-friendly version number
+$plugin->release   = "1.0.2"; // User-friendly version number
 $plugin->dependencies = array(
     'local_evento' => 2017071302
 );
