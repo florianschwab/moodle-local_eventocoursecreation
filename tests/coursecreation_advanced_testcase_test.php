8d1f811d38a52795b96c4839bc0226eb7c002403
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-11-21 09:15:20 +0100

    Update coursecreation_advanced_testcase_test.php

--- tests/coursecreation_advanced_testcase_test.php
+++ tests/coursecreation_advanced_testcase_test.php
@@ -109,16 +109,16 @@ class mod_evento_advanced_testcase extends advanced_testcase {
      * @test
      */
      public function basic() {
-         $anlass = $this->simulator->get_event_by_number("mod.mmpAUKATE1.HS18_BS.001");
-         var_dump($this->simulator);
-//         var_dump($anlass);
-         $personenanmeldung = $this->simulator->get_enrolments_by_eventid(25490);
-         $personenanmeldung = $this->simulator->get_enrolments_by_eventid(25490);
+         $anlass = $this->simulator->get_event_by_number("mod.mmpAUKATE1.HS18_BS.002");
+//         var_dump($this->simulator);
+         var_dump($anlass);
+//         $personenanmeldung = $this->simulator->get_enrolments_by_eventid(25490);
+//         $personenanmeldung = $this->simulator->get_enrolments_by_eventid(25490);
 
-         $person = $this->simulator->get_person_by_id(141703);
-         $person = $this->simulator->get_person_by_id(117828);
-         $ad_account = $this->simulator->get_ad_accounts_by_evento_personid(141701, null, null);
-         $ad_account_student = $this->simulator->get_all_ad_accounts(null);
+//         $person = $this->simulator->get_person_by_id(141703);
+//         $person = $this->simulator->get_person_by_id(117828);
+//         $ad_account = $this->simulator->get_ad_accounts_by_evento_personid(141701, null, null);
+//         $ad_account_student = $this->simulator->get_all_ad_accounts(null);
      }
 
 }
