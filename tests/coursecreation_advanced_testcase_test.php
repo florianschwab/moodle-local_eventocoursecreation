a95e2da7771f2271fe83f001417988c027652fdc
Wieling Thomas <wielinthoma1@htwchur.ch>
2019-11-21 09:34:10 +0100

    Update coursecreation_advanced_testcase_test.php

--- tests/coursecreation_advanced_testcase_test.php
+++ tests/coursecreation_advanced_testcase_test.php
@@ -109,10 +109,10 @@ class mod_evento_advanced_testcase extends advanced_testcase {
      * @test
      */
      public function basic() {
-         $anlass = $this->simulator->get_event_by_number("mod.mmpAUKATE1.HS18_BS.002");
+//         $anlass = $this->simulator->get_event_by_number("mod.mmpAUKATE1.HS18_BS.002");
 //         var_dump($this->simulator);
 var_dump("ANLASS ANFANG");
-         var_dump($anlass);
+//         var_dump($anlass);
          var_dump("ANLASS ENDE");
 //         $personenanmeldung = $this->simulator->get_enrolments_by_eventid(25490);
 //         $personenanmeldung = $this->simulator->get_enrolments_by_eventid(25490);
